569dc08de3106614b70b36467500702fb112e48a
Crowdbotics <team@crowdbotics.com>
2021-09-23 04:35:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "health_care_system_30765Identifier",
+  url: "https://health_care_system_30765Identifier.botics.co",
+  api: "https://health_care_system_30765Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
